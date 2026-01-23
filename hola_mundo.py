93541d3c8a5feedfb99053206adc1192b593ed1c
Santiago Marin <santiago.marinb@upb.edu.co>
2026-01-23 15:00:07 -0500

    Cambios Python

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,17 @@
-# MI primer script.py
+# mi_primer_script.py
 print("¡Hola, GitHub!")
+
+# Calcular números primos entre 1 y 1000
+def es_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+primos = [num for num in range(1, 1001) if es_primo(num)]
+print("Números primos entre 1 y 1000:")
+print(primos)
+
+
